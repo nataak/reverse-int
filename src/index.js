affa26f23894ff835006f39b5dae581c99e23121
Nata <nata.ak848@gmail.com>
2021-07-27 16:18:48 +0400

    feat: pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let number = Math.abs(n);
+    let arr = number.toString().split("");
+    let reversed = arr.reverse().join("");
+    return reversed;
 }
+
+
+
+
+
